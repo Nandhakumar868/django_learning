d160bf1e62d4e8697ba8e9043d8d206db4096054
Nandhakumar <kumarnandha8983@gmail.com>
2024-02-05 10:30:05 +0530

    Modified

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,12 @@
+from .models import Choice, Question, Member
 from django.contrib import admin
 
 # Register your models here.
+
+
+admin.site.register(Question)
+admin.site.register(Choice)
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+  
+admin.site.register(Member, MemberAdmin)
--- my_app/models.py
+++ my_app/models.py
@@ -20,4 +20,10 @@ class Choice(models.Model):
         return self.choice_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  phone = models.IntegerField(null=True)
+  joined_date = models.DateField(null=True)
 
