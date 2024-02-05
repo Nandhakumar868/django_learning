780ca4cee4c4fdadf29a5f6f7726a157dd856026
Nandhakumar <kumarnandha8983@gmail.com>
2024-02-05 08:16:53 +0530

    files modified

--- my_firstproject/settings.py
+++ my_firstproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'my_firstproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'first_database',
+        'USER': 'postgres',
+        'PASSWORD': 'postgreflow@123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
